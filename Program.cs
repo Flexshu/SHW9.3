993eb3c6e5d150572023d60629aec0bfd0ff9c6f
Flexshu <egor.wm202@gmail.com>
2025-08-30 11:49:08 +0300

    V1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DelegatesDemo
+{
+    class Program3
+    {
+        static void Main()
+        {
+            string[] words = { "hello", "world", "delegates" };
+
+            Action<string> upper = s => Console.WriteLine(s.ToUpper());
+            Action<string> length = s => Console.WriteLine("Length: " + s.Length);
+            Action<string> decorate = s => Console.WriteLine("*" + s + "*");
+
+            Action<string> combined = upper + length + decorate;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine("Word: " + words[i]);
+                combined(words[i]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
